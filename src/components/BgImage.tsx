0b7583c95f8b3ab9afac701be9e679cb9963fa70
iVeluu <velualex10@gmail.com>
2024-10-04 00:16:12 -0600

    adding pictures, svgs, background

---
+++ src/components/BgImage.tsx
@@ -0,0 +1,18 @@
+
+export default function BgImage() {
+    return (
+        <div
+            style={{
+                width: '100vw',
+                height: '100vh',
+                backgroundColor: '#20293A',
+                backgroundImage: 'url(/hero-image-github-profile.png)',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: '100vw 30vh',
+                overflowX: 'hidden',
+            }}
+        >
+
+        </div>
+    )
+}
